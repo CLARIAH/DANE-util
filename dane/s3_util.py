fa290bb0de8139fcb80c9ea6311638d48477c174
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2024-07-26 10:52:27 +0200

    Handle non-tar output transfer: parse the file_list into files and key. Also, empty prefix in bucket before uploading, and improved download code. Added some docstrings as wel

--- dane/s3_util.py
+++ dane/s3_util.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import boto3
 import logging
 import ntpath
@@ -5,17 +6,43 @@ import os
 from pathlib import Path
 import tarfile
 from typing import List, Tuple, Optional
-
+from time import time
+from dane.provenance import Provenance
 
 logger = logging.getLogger("DANE")
 COMPRESSED_TAR_EXTENSION = ".tar.gz"
 
 
-# the file name without extension is used as an asset ID by the ASR container to save the results
+@dataclass
+class DownloadResult:
+    success: bool
+    file_path: str  # target_file_path,  # TODO harmonize with dane-download-worker
+    download_time: float = -1  # time (msecs) taken to receive data after request
+    mime_type: str = "unknown"  # download_data.get("mime_type", "unknown"),
+    content_length: int = -1  # download_data.get("content_length", -1),
+
+    def to_download_provenance(
+        self,
+        input_file_path: str,
+        start_time: float,
+        software_version: str,
+    ) -> Provenance:
+        return Provenance(
+            activity_name="Download S3 data",
+            activity_description="Download data from S3 bucket",
+            start_time_unix=start_time,
+            processing_time_ms=self.download_time,
+            software_version=software_version,
+            input_data={"input_file_path": input_file_path},
+            output_data={"file_path": self.file_path},
+        )
+
+
+# the file name without extension is used as an asset ID to save the results
 def generate_asset_id_from_input_file(
     input_file: str, with_extension: bool = False
 ) -> str:
-    logger.info(f"generating asset ID for {input_file}")
+    logger.debug(f"generating asset ID for {input_file}")
     file_name = ntpath.basename(input_file)  # grab the file_name from the path
     if with_extension:
         return file_name
@@ -32,10 +59,58 @@ def is_valid_tar_path(archive_path: str) -> bool:
         return False
     if archive_path[-7:] != COMPRESSED_TAR_EXTENSION:
         logger.error(
-            f"Archive file should have the correct extension: {COMPRESSED_TAR_EXTENSION}"
+            f"Archive file should have correct extension: {COMPRESSED_TAR_EXTENSION}"
         )
         return False
     return True
+
+
+def digest_file_list(
+    file_list: List[str], prefix: str, tar_archive_path: str = ""
+) -> List[Tuple[str, str]]:
+    """
+    Go over the file list and return a list of (filepath, key) tuples for S3 transfer.
+    The key a combination of the specified prefix and the basename of the file.
+    If tar_archive_path is given, the content of the file_list is tarred and a singleton
+    list of (tar_archive, key) is returned.
+    If not, the file list (which may contain directories) is processed recursively,
+    adding each (file, key). Directory structure is maintained as infix.
+    """
+    
+    # first check if the file_list needs to be compressed (into tar)
+    if tar_archive_path:
+        tar_success = tar_list_of_files(tar_archive_path, file_list)
+        if not tar_success:
+            logger.error("Could not archive the file list before transferring to S3")
+            raise Exception("Could not digest file list: archive error")
+        key = os.path.join(
+            prefix,
+            generate_asset_id_from_input_file(  # file name with extension
+                tar_archive_path, True
+            ),
+        )
+        file_and_key_list = [(tar_archive_path, key)]
+    else:
+        file_and_key_list = []
+        for f in file_list:
+            if os.path.isdir(f):
+                for root, dirs, files in os.walk(f):
+                    file_and_key_list.extend(
+                        digest_file_list(
+                            file_list=[os.path.join(root, fn) for fn in dirs + files],
+                            prefix=os.path.join(prefix, os.path.basename(f)),
+                            tar_archive_path=tar_archive_path,
+                        )
+                    )
+            else:
+                key = os.path.join(
+                    prefix,
+                    generate_asset_id_from_input_file(  # file name with extension
+                        f, True
+                    ),
+                )
+                file_and_key_list.append((f, key))
+    return file_and_key_list
 
 
 def tar_list_of_files(archive_path: str, file_list: List[str]) -> bool:
@@ -78,12 +153,14 @@ def parse_s3_uri(s3_uri: str) -> Tuple[str, str]:
     return bucket, object_name
 
 
-def download_s3_uri(s3_uri: str, output_folder: str) -> bool:
+def download_s3_uri(s3_uri: str, output_folder: str) -> DownloadResult:
+    logger.info(f"Downloading {s3_uri}")
     if not validate_s3_uri(s3_uri):
         logger.error("Invalid S3 URI")
-        return False
+        return DownloadResult(success=False, file_path="")
     s3_store = S3Store()
     bucket, object_name = parse_s3_uri(s3_uri)
+    logger.debug(f"OBJECT NAME: {object_name}")
     return s3_store.download_file(bucket, object_name, output_folder)
 
 
@@ -101,39 +178,56 @@ class S3Store:
     def __init__(self, s3_endpoint_url: Optional[str] = None, unit_testing=False):
         self.client = boto3.client("s3", endpoint_url=s3_endpoint_url)
 
-    def transfer_to_s3(
-        self, bucket: str, path: str, file_list: List[str], tar_archive_path: str = ""
-    ) -> bool:
-        # first check if the file_list needs to be compressed (into tar)
-        if tar_archive_path:
-            tar_location = tar_list_of_files(tar_archive_path, file_list)
-            if not tar_location:
-                logger.error(
-                    "Could not archive the file list before transferring to S3"
-                )
-                return False
+    def clear_prefix(self, bucket: str, prefix: str):
+        """
+        Clean up the prefix in the bucket: delete all keys with the specified prefix.
+        """
+        response = self.client.list_objects(Bucket=bucket, Prefix=prefix)
+        if len(response['Contents'])>0:
+            logger.warn(f"Prefix '{prefix}' exists in bucket '{bucket}'.")                        
+            to_delete = []
+            while True:
+                to_delete += [{'Key': item['Key']} for item in  response['Contents']]
+                if not response['IsTruncated']:
+                    break
+                response = self.client.list_objects(Bucket=bucket, Prefix=prefix, Marker=response['Contents'][-1]['Key'])            
+            logger.warn(f"Removing {len(to_delete)} item with '{prefix}' as prefix from bucket.")
+            self.client.delete_objects(Bucket=bucket, Delete={'Objects': to_delete})
+        # TODO: Add error handling
 
-            file_list = [tar_archive_path]  # now the file_list just has the tar
+    def transfer_to_s3(
+        self, bucket: str, prefix: str, file_list: List[str], tar_archive_path: str = ""
+    ) -> bool:
+        """
+        Transfer the contents of file_list to the specified S3 bucket with the
+        specified prefix.
+        If tar_archive_path is specified, the file_list is wrapped into a single
+        archive first. Otherwise, the directory structure in file_list is maintained
+        in the prefix.
+        """
+        self.clear_prefix(bucket, prefix) # clean up any old stuff
+        file_and_key_list = digest_file_list(
+            file_list=file_list,
+            prefix=prefix,
+            tar_archive_path=tar_archive_path)
 
         # now go ahead and upload whatever is in the file list
-        for f in file_list:
+        for f, k in file_and_key_list:
             try:
                 self.client.upload_file(
                     Filename=f,
                     Bucket=bucket,
-                    Key=os.path.join(
-                        path,
-                        generate_asset_id_from_input_file(  # file name with extension
-                            f, True
-                        ),
-                    ),
+                    Key=k,
                 )
-            except Exception:  # TODO figure out which Exception to catch specifically
+            except Exception:  # TODO figure out which Exception(s) to catch specifically
                 logger.exception(f"Failed to upload {f}")
                 return False
         return True
 
-    def download_file(self, bucket: str, object_name: str, output_folder: str) -> bool:
+    def download_file(
+        self, bucket: str, object_name: str, output_folder: str
+    ) -> DownloadResult:
+        start_time = time()
         logger.info(f"Downloading {bucket}:{object_name} into {output_folder}")
         if not os.path.exists(output_folder):
             logger.info("Output folder does not exist, creating it...")
@@ -141,8 +235,12 @@ class S3Store:
         output_file = os.path.join(output_folder, os.path.basename(object_name))
         try:
             with open(output_file, "wb") as f:
-                self.client.download_fileobj(bucket, object_name, f)
+                success = self.client.download_fileobj(bucket, object_name, f)
         except Exception:
             logger.exception(f"Failed to download {object_name}")
-            return False
-        return True
+
+        return DownloadResult(
+            success=success,
+            file_path=output_file,
+            download_time=time() - start_time,
+        )
